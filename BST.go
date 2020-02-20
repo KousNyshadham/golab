e54171ba1b49bce5c9444a905883f7b29c63f0b2
Kous <kous@Kouss-MacBook-Air.local>
2020-02-20 02:01:04 -0600

    more

--- BST.go
+++ BST.go
@@ -6,11 +6,25 @@ import (
 	"io/ioutil"
 	"strings"
 	"strconv"
+	"sync"
 )
+
+type hashId struct{
+	hash int
+	id int
+}
+
+var dataWorkers int
+var hashComparision int
+var hashPtr *int
+var dataPtr *int
+var compPtr *int
+var h2i = make(map[int][]int)		
 
 type Tree struct{
 	root *Node
 }
+
 
 type Node struct{
 	Val int
@@ -26,7 +40,8 @@ func in_order_traversal(node *Node, traversal* [] int){
 	}
 }
 
-func (tree *Tree) computeHash() int{
+
+func sequentialComputeHash(tree *Tree) int{
 	hash := 1
 	var inorder [] int
 	in_order_traversal(tree.root, &inorder)
@@ -35,6 +50,23 @@ func (tree *Tree) computeHash() int{
 		hash = (hash * new_value + new_value) % 4222234741
 	}
 	return hash
+}
+
+func parallelComputeHash(tree *Tree, id int, wg *sync.WaitGroup,c chan hashId){
+	hash := 1
+	var inorder [] int
+	in_order_traversal(tree.root, &inorder)
+	for i := 0; i < len(inorder); i++ {
+		new_value := inorder[i] + 2
+		hash = (hash * new_value + new_value) % 4222234741
+	}
+	if dataWorkers == 1 {
+		treeInMap := hashId{hash, id}
+		c <- treeInMap
+	} else if dataWorkers == hashComparision{
+
+	}
+	wg.Done()
 }
 
 func (node *Node) insert(val int){
@@ -53,16 +85,20 @@ func (node *Node) insert(val int){
 	}
 }
 
+func mapInsert(c chan hashId, raceCondition chan int, lenTree int){
+	for pair:=range c {
+		h2i[pair.hash] = append(h2i[pair.hash], pair.id)
+	}
+	raceCondition <- 1 // ensures everything is added to map before printing
+}
+
 func main() {
 	hashPtr := flag.Int("hash-workers", 0, "a int")
 	dataPtr := flag.Int("data-workers", 0, "a int")
 	compPtr := flag.Int("comp-workers", 0, "a int")
 	inputPtr := flag.String("input", "", "a string")
 	flag.Parse()
-	fmt.Println("hash:", *hashPtr)
-	fmt.Println("data:", *dataPtr)
 	fmt.Println("comp:", *compPtr)
-	fmt.Println("input:", *inputPtr)
 	content, err3 := ioutil.ReadFile(*inputPtr)
 	if err3 != nil{
 		fmt.Println(err3)
@@ -81,11 +117,36 @@ func main() {
 		}
 		trees = append(trees, tree)
 	}
-
-	var hashes [] int
-	for i := 0; i < len(trees); i++ {
-		hash :=  trees[i].computeHash()
-		fmt.Printf("%d\n", hash)
-		hashes = append(hashes, hash)
+	var hashWorkers = *hashPtr
+	hashComparision = *hashPtr
+	dataWorkers = *dataPtr
+	c := make(chan hashId)
+	raceCondition := make(chan int)
+	if hashWorkers != 1 && *dataPtr == 1 {
+		go mapInsert(c, raceCondition, len(splitTrees))
 	}
+	i := 0
+	for i < len(trees){
+		var leftToTraverse = len(trees)-i
+		if *hashPtr == 1 {
+			hash := sequentialComputeHash(&trees[i])
+			h2i[hash] = append(h2i[hash], i)
+			i += 1
+		} else {
+			if leftToTraverse < hashWorkers {
+				hashWorkers = leftToTraverse
+			}
+			var wg sync.WaitGroup	
+			for j :=0; j < hashWorkers; j++{
+				wg.Add(1)
+				go parallelComputeHash(&trees[i+j], i+j, &wg,c)
+
+			}
+			i+=hashWorkers
+			wg.Wait()
+		}
+	}
+	close(c)
+	<-raceCondition
+	fmt.Println(h2i)
 }
